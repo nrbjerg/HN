a415348e9c0665bad26cda3d7d02e57224e3040c
nrbjerg <mart547n@outlook.dk>
2023-10-08 18:06:38 +0200

    Created an initial matrix generator.

--- src/matrix_generator.py
+++ src/matrix_generator.py
@@ -1,19 +1,43 @@
 import galois as gl
 import numpy as np
 from colorama import Back, Style
+from typing import Iterable, Tuple
+import math
+
+def convert_int_to_symbols (n: int, gf: gl.GF, length: int) -> gl.FieldArray:
+    """Convert n to a set of symbols."""
+    if n > gf.order ** length:
+        raise ValueError(f"Cannot convert {n} to {length} symbols of GF({gf.properties.order})")
+
+    res = n
+    symbols = [0 for _ in range(length)]
+    for idx in range(length, 1, -1):
+        symbols[idx - 2] = math.floor(n / (gf.order ** idx))
+        res = n % (gf.order ** idx)
+        if res == 0:
+            break
+
+    symbols[-1] = res
+
+    vals = [int(c) for c in str(int(str(n), base=gf.order))]
+    print(vals)
+    return gf(vals)
+
+def window(data: gl.FieldArray, window_size: int) -> Iterable[Tuple[int]]:
+    """A generator yielding disjoint windows of the data array."""
+    for idx in range(data.shape[0] // window_size):
+        yield tuple(data[idx * window_size : (idx + 1) * window_size])
 
 def number_of_symbols_in_HN_code (size: int) -> int:
     """Computes the expected number of symbols in a HN code."""
-    return size ** 2 - 2 * (8 * 10) + (6 * 8) + 2 * (size - 16)
+    size_of_small_locator = 6 * 8
+    size_of_big_locator = 8 * 8
+    strip_size = size - 6 - 8
+    return size ** 2 - size_of_big_locator - 2 * size_of_small_locator - 8 * (strip_size)
 
-def generate_matrix(data: gl.FieldArray, error_correction_level: int, mask_index: int, encoding: int, base: int = 8, size: int = 24) -> gl.FieldArray:
-    """Creates a matrix representation of the HN code, given  the data array."""
-    # 1. Check that we have recived the correct number of symbols
-    number_of_symbols = number_of_symbols_in_HN_code(size)
-    if data.shape[0] != number_of_symbols:
-        raise ValueError(f"Expected data array to have size {number_of_symbols} but got {data.shape[0]}")
-
-    gf = gl.GF(base)
+def setup_locator_and_timing_pattern(gf: gl.GF, size: int) -> gl.FieldArray:
+    """Create a matrix with the locator and timing patterns."""
+    # 1. Construct matrix
     matrix = gf.Ones((size + 2, size + 2))
 
     # 2. Add locators to matrix
@@ -21,38 +45,63 @@ def generate_matrix(data: gl.FieldArray, error_correction_level: int, mask_index
     small_locator = gf.Zeros((5, 5)) - gf(np.pad(gf.Ones((3, 3)), 1)) + gf(np.pad(gf.Ones((1, 1)), 2))
 
     matrix[1:8, 1:8] = large_locator # NOTE: Remember that we have padding around the edge of the code.
-    matrix[1:8, -8:-1] = large_locator
+    matrix[1:6, -6:-1] = small_locator
     matrix[-6:-1, 1:6] = small_locator
 
     # 3. Add timing information
-    timing_pattern = gf([2 for _ in range(size - 16)]) # [(idx + 1) % 2 for idx in range(size - 16)])
-    matrix[6, 9:-9] = timing_pattern
-    matrix[11:-7, 6] = timing_pattern
+    timing_pattern = gf([idx % 2 for idx in range(size - 8 - 6)])
+    matrix[7, 9:-7] = timing_pattern
+    matrix[9:-7, 7] = timing_pattern
 
+    return matrix
+
+def add_parameters_and_calibration_colors(gf: gl.GF, matrix: gl.FieldArray, error_correction_level: int, mask: int, encoding: int) -> gl.FieldArray:
+    """Adds parameters and calibration colors to the matrix"""
     # 4. Add calibration symbols:
-    colors = gf([3, 3, 3])# [2, 3, 5])
-    matrix[-4:-1, 7] = colors
-    matrix[9, 2:5] = colors
-    matrix[9, -5:-2] = colors
+    rgb = gf([2, 3, 5])
+    for pos in [((7, 7, 7), (-4, -3, -2)), ((6, 6, 6), (12, 13, 14)), ((12, 13, 14), (6, 6, 6)), ((-4, -3, -2), (7, 7, 7))]:
+        matrix[pos] = rgb
 
-    # 5. Add extra information
-    matrix[9, 1], matrix[9, 8] = encoding, encoding
-    matrix[9, -9] = encoding
+    for pos in [(9, 6), (-8, 6), (6, 15)]:
+        matrix[pos] = error_correction_level
 
-    mask_symbols = (mask_index // base, mask_index % base) # TODO: use this
-    matrix[9, 7], matrix[9, 6] = mask_index, mask_index
-    matrix[9, -8], matrix[9, -7] = mask_index, mask_index
-    matrix[-5, 7], matrix[-6, 7] = mask_index, mask_index
+    masking_symbols = convert_int_to_symbols(mask, gf, length = 2)
+    for pos in [((6, 6), (-8, -9)), ((10, 11), (6, 6)), ((-5, -6), (7, 7))]:
+        matrix[pos] = masking_symbols
 
-    matrix[9, 5] = error_correction_level
-    matrix[9, -6] = error_correction_level
-    matrix[9, -2] = error_correction_level
+    encoding_symbols = convert_int_to_symbols(encoding, gf, length = 2)
+    for pos in [((6, 6), (10, 11)), ((7, 7), (-5, -6)), ((-9, -10), (6, 6))]:
+        matrix[pos] = encoding_symbols
 
+    return matrix
+
+
+def add_data (gf: gl.GF, matrix: gl.FieldArray, data: gl.FieldArray) -> gl.FieldArray:
+    """adds the given data to the """
+    print(data.shape)
+    a, b, c = np.split(data, [15 * 15, 15 * 15 + 4 * 9])
+    print(a.shape, b.shape, c.shape, matrix[-18:-8, 1:5].shape)
+    matrix[-16:-1, -16:-1] = np.reshape(a, (15, 15))
+    matrix[-16:-7, 1:5] = np.reshape(b, (9, 4))
+    matrix[1:5, -16:-7] = np.reshape(c, (9, 4)).T
+    return matrix
+
+
+def generate_matrix(data: gl.FieldArray, gf: gl.GF, error_correction_level: int, mask: int, encoding: int, size: int = 23) -> gl.FieldArray:
+    """Create a matrix representation of the HN code, given the data array."""
+    # 1. Check that we have recived the correct number of symbols
+    number_of_symbols = number_of_symbols_in_HN_code(size)
+    if data.shape[0] != number_of_symbols:
+        raise ValueError(f"Expected data array to have size {number_of_symbols} but got {data.shape[0]}")
+
+    matrix = setup_locator_and_timing_pattern(gf, size)
+    matrix = add_parameters_and_calibration_colors(gf, matrix, error_correction_level, mask, encoding)
+    matrix = add_data(gf, matrix, data)
     return repr_matrix(matrix)
 
 def repr_matrix(mat: gl.FieldArray) -> str:
     """Allows us to print a QR style code to the terminal."""
-    color_mapping = {0: Style.RESET_ALL + "  ", 1: Back.WHITE + "  ", 2: Back.RED + "  ", 3: Back.GREEN + "  ", 4: Back.BLUE + "  ", 5: Back.MAGENTA + "  ", 6: Back.YELLOW + "  "}
+    color_mapping = {0: Style.RESET_ALL + "  ", 1: Back.WHITE + "  ", 2: Back.RED + "  ", 3: Back.GREEN + "  ", 4: Back.YELLOW + "  ", 5: Back.BLUE + "  ", 6: Back.MAGENTA + "  ", 7: Back.CYAN + "  "}
     rows = ["".join((color_mapping[int(cell)] for cell in row)) for row in mat]
     buffer = "\n " + f"{Style.RESET_ALL}\n ".join(rows) + f"{Style.RESET_ALL}\n"
 
@@ -60,4 +109,6 @@ def repr_matrix(mat: gl.FieldArray) -> str:
 
 
 if __name__ == "__main__":
-    print(generate_matrix(np.array([0 for _ in range(number_of_symbols_in_HN_code(24))]), 4, 5, 6))
+    gf = gl.GF(8)
+    print(convert_int_to_symbols(12, gf, 2))
+    print(generate_matrix(gf.Random(shape=(number_of_symbols_in_HN_code(23),)), gf, encoding = 6, error_correction_level = 4, mask = 7))
